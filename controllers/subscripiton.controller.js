18fb9fd3175ac7e00555ceabb56cba62024036ac
Bilal <mobilal560@gmail.com>
2025-02-24 22:56:19 +0530

    add upstash config and fixed typos

--- controllers/subscripiton.controller.js
+++ controllers/subscripiton.controller.js
@@ -1,70 +1,70 @@
-import Subscription from '../models/subscription.model.js'
+import { SERVER_URL } from "../config/env.js";
+import { workflowClient } from "../config/upstash.js";
+import Subscription from "../models/subscription.model.js";
 
+export const createSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.create({
+      ...req.body,
+      user: req.user._id,
+    });
 
+   const {workflowRunId} = await workflowClient.trigger({
+        url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+        body: {
+            subscriptionId: subscription.id,
+        },
+        headers: {
+            'content-type': 'application/json'
+        },
 
-export const createSubscription =  async (req, res, next) => {
-    try{
+        retries: 0,
+    })
 
-        const subscription =  await Subscription.create({
-            ...req.body,
-            user: req.user._id
-        });
+    res.status(201).json({ success: true, data:{ subscription,  workflowRunId } });
+  } catch (error) {
+    next(error);
+  }
+};
 
-        res.status(201).json({success: true, data: subscription})
-
+export const getUserSubscriptions = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const error = new Error("Your are not the owner  of this account");
+      error.statusCode = 401;
+      throw error;
     }
 
-    catch(error){
-        next(error)
-    }
-}
+    const subscripiton = await Subscription.find({ user: req.params.id });
 
-export const  getUserSubscriptions = async (req, res, next) => {
-    try{
-        if(req.user.id !== req.params.id){
-            const error = new Error("Your are not the owner  of this account");
-            error.statusCode = 401;
-            throw error
-        }
-
-        const subscripiton  = await Subscription.find({user: req.params.id})
-
-        res.status(200).json({success: true, data: subscripiton, })
-    }
-
-    catch(error)
-    {
-        next(error)
-    }
-}
+    res.status(200).json({ success: true, data: subscripiton });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getAllSubscriptions = async (req, res, next) => {
-    try{
-         const allSubscriptions = await Subscription.find()
-         
-         res.status(200).json({success: true, data: allSubscriptions})
-    }
+  try {
+    const allSubscriptions = await Subscription.find();
 
-    catch(error){
-        next(error)
-    }
-}
+    res.status(200).json({ success: true, data: allSubscriptions });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getSubscriptionDetails = async (req, res, next) => {
-    try {
+  try {
+    const subscriptionsDetail = await Subscription.findById(req.params.id);
 
-        const subscriptionsDetail = await Subscription.findById(req.params.id)
-
-        if(!subscriptionsDetail){
-            const error = new Error("can not find subscription details")
-            error.status = 404;
-            throw error
-        }
-         
-        res.status(200).json({success: true, data: subscriptionsDetail })
-
-    } catch (error) {
-        next(error)
+    if (!subscriptionsDetail) {
+      const error = new Error("can not find subscription details");
+      error.status = 404;
+      throw error;
     }
-}   
 
+    res.status(200).json({ success: true, data: subscriptionsDetail });
+  } catch (error) {
+    next(error);
+  }
+};
--- middleware/arcjet.middleware.js
+++ middleware/arcjet.middleware.js
@@ -1,27 +1,23 @@
 import aj from "../config/arcjet.js";
 
-
 const arcjetMiddleware = async (req, res, next) => {
-    
-    try {
-        
-        const decision = await aj.protect(req, { requested: 1})
+  try {
+    const decision = await aj.protect(req, { requested: 1 });
 
-        if(decision.isDenied())
-        {
-            if(decision.reason.isRateLimit()) return res.status(429).json({error: "Rate limit Exceeded"})
-            if(decision.reason.isBot()) return res.status(403).json({error: "Bot Detected"})
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit())
+        return res.status(429).json({ error: "Rate limit Exceeded" });
+      if (decision.reason.isBot())
+        return res.status(403).json({ error: "Bot Detected" });
 
-            return res.status(403).json({eror: "Access Denied"})
-        }
-         
-        next()
-
-    } catch (error) {
-        console.error(`Arcjet middleware Error: ${error}`)   
-        next(error)
+      return res.status(403).json({ eror: "Access Denied" });
     }
 
-}
+    next();
+  } catch (error) {
+    console.error(`Arcjet middleware Error: ${error}`);
+    next(error);
+  }
+};
 
-export default arcjetMiddleware
+export default arcjetMiddleware;
